bacf79b91b41672f9cdb33ba45a0261a4f0a8a77
Philip Cohen <philip.cohen@f1006-24-34170.bmcc.cuny.edu>
2017-10-16 17:02:14 -0400

    Functions

---
+++ Functions/app.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", function() {
+    var userName = "Philip";
+    var c = document.getElementById("container");
+            	var submit = document.getElementById("submit-message");
+	var input = document.getElementById("new-message");
+	submit.addEventListener("click", addMessage);
+	input.addEventListener("keydown", function(event) {
+		if (event.key == "Enter") {
+			addMessage();
+		}
+	});
+	
+	function addMessage() {
+		var msg = document.createElement("div");
+		var text = document.createElement("p");
+		var user = document.createElement("p");
+		msg.className = "message";
+		text.innerText = input.value;
+		text.className = "text";
+		user.innerText = userName;
+		user.className = "user";
+		msg.appendChild(text);
+		msg.appendChild(user);
+		
+		var del = document.createElement("button");
+		del.className = "delete";
+		del.innerText = "Delete";
+		del.addEventListener("click", function() {
+			this.parentNode.style.backgroundColor = "Gold";
+		});
+		
+		
+		
+		msg.appendChild(del);
+		
+		
+		
+		c.appendChild(msg);
+	}
+
+});
