93516ddc60f78890aa4075eac99ef634167d8ee5
Philip Cohen <philip.cohen@f1006-24-34170.bmcc.cuny.edu>
2017-10-23 15:03:11 -0400

    New Quiz

--- Quiz/app.js
+++ Quiz/app.js
@@ -1,33 +1,56 @@
-<<<<<<< HEAD
+var Bowser = { 
+    question: "What is the name of Nintendo's Italian plumber?",
+    answer: "Mario", 
+    image: "Mario.jpg"
+};
+
+var Zelda = {
+    question: "Who is the young man who recues Princess Zelda?",
+    answer: "Link",
+    image: "Link.jpg"
+};
+
+var Pokemon = {
+    question: "What is the name of everybodys favorite electric mouse from Pokemon?",
+    answer: "Pikachu",
+    image: "Pikachu.jpg"
+};
+
+var Metroid = {
+    question: "Who wears a body of armor to hunt down metroids?",
+    answer: "Samus",
+    image: "Samus.jpg"
+};
+
 var submit = document.getElementById("submit");
-=======
- var submit = document.getElementById("submit");
->>>>>>> 3b29edbd0e7e6960f43f7b3ab1c62ce2ffc36bc3
 submit.onclick = function(event) {
     var count = 0;
-    var Mario = document.getElementById("Question1");
-    var answer = "Mario";
-    if (Mario.value == answer) {
+    function correctAnswer(q) {
         count++;
+        var img = new Image();
+        img.src = q.image;
+        document.body.appendChild(img);
+    }
+    
+    var Mario = document.getElementById("Question1");
+    if (Mario.value == Bowser.answer) {
+        correctAnswer(Bowser);
     }
 
-var Link = document.getElementById("Question2");
-     var answer = "Link";
-    if (Link.value == answer) {
-        count++;
+    var Link = document.getElementById("Question2");
+    if (Link.value == Zelda.answer) {
+         correctAnswer(Zelda);
     }
-var Pikachu = document.getElementById("Question3");
-     var answer = "Pikachu";
-    if (Pikachu.value == answer) {
-        count++;
+    var Pikachu = document.getElementById("Question3");
+    if (Pikachu.value == Pokemon.answer) {
+         correctAnswer(Pokemon);
     }
-var Samus = document.getElementById("Question4");
-     var answer = "Samus";
-    if (Samus.value == answer) {
-        count++;
+    var Samus = document.getElementById("Question4");
+    if (Samus.value == Metroid.answer) {
+         correctAnswer(Metroid);
     }
     var message = document.createElement("p");
     message.innerText = "Congrats you know your nintendo star players. You got "+count+" out of 4.";
     
-document.body.appendChild(message);
+    document.body.appendChild(message);
 }
